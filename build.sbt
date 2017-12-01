f6e1c0d1baefcdc61455fde9c988ea1d505f6d75
Hypnoes <hypnoes@qq.com>
2017-12-01 22:35:11 +0800

    Init with main.scala

--- build.sbt
+++ build.sbt
@@ -1,5 +1,10 @@
 lazy val root = (project in file("."))
     .settings(
         name := "twaddle",
-        scalaVersion := "2.12.3"
+        version := "dev-0.00",
+        scalaVersion := "2.12.3",
+        libraryDependencies ++= Seq(
+            "org.apache.spark" % "spark-core-2.11" % "2.2.0",
+            "org.apache.spark" % "spark-sql_2.11" % "2.2.0"
+        )
     )
--- src/main/main.scala
+++ src/main/main.scala
@@ -1,7 +1,20 @@
-import scala.math._
+import org.apache.spark.sql.SparkSession
 
 object Main {
-    def main(args: Array[String]): Unit = {
-        // To do here.
+    def main(args: Array[String]) {
+        val spark = SparkSession
+            .builder
+            .appName("Simple Application")
+            .getOrCreate()
+
+        val logFile = "path/to/logfile"
+        val logData = spark.read.textFile(logFile).cache()
+
+        val numAs = logData.filter(line => line.contains("a")).count()
+        val numBs = logData.filter(line => line.contains("b")).count()
+
+        println(s"Lines with a: $numAs, Lines with b: $numBs")
+        
+        spark.stop()
     }
 }
